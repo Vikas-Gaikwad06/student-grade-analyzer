04a7ebd15762b60f0b5b36bb8f21deb604909cc4
Vikas-Gaikwad06 <vikassgaikwad06@gmail.com>
2025-07-03 18:02:16 +0530

    Add files via upload

---
+++ Exceptionhand.py
@@ -0,0 +1,49 @@
+def calsi():
+    try:
+        results = []
+        while True:
+            n1 = int(input("Enter the first number: "))
+            n2 = int(input("Enter the second number: "))
+            operator = input("Enter the operator (+, -, *, /): ")
+
+            
+
+            if operator == "+":
+                result = n1 + n2
+            elif operator == "-":
+                # print("Result:", n1 - n2)
+                result = n1 - n2
+            elif operator == "*":
+                # print("Result:", n1 * n2)
+                result = n1 * n2
+            elif operator == "/":
+                if n2 != 0:
+                    # print("Result:", n1 / n2)
+                    result = n1 / n2
+                else:
+                    print("Error: Division by zero is not allowed.")
+
+            else:
+                print("Invalid operator. Please enter one of +, -, *, /")
+                continue  # Ask agai
+
+
+            # print("Result history: ", result)
+            results.append(result)
+
+            # Ask usery if they want to continue
+            user_input = input("Do you want to continue? (y/n): ").lower()
+            if user_input == 'n':
+                print("Thanks for using the calculator.")
+                print("The result history is: ",results)
+                
+                with open("file.java", "w") as file:
+                    file.write(str(results))
+
+                break
+    
+    except ValueError:
+        print("Invalid input. Please enter integer values.")
+
+calsi()
+
